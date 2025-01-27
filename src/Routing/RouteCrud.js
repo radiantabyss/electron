853d9173cf09ddf9ea2588fc62e1d234bdd9b075
Robert Buchi <robert@planetweb.ro>
2025-01-27 21:00:41 +0200

    route crud fix

--- src/Routing/RouteCrud.js
+++ src/Routing/RouteCrud.js
@@ -3,10 +3,14 @@ let self = {
         domain = domain.replace(/\\/g, '.').replace(/\//g, '.');
         let prefix = Str.kebab(domain).replace(/\./g, '/');
 
-        Route.get(`/${prefix}`, `${domain}.ListAction`, [], '', false);
-        Route.get(`/${prefix}/new/{id?}`, `${domain}.NewAction`, [], '', false);
-        Route.get(`/${prefix}/edit/{id}`, `${domain}.EditAction`, [], '', false);
-        Route.get(`/${prefix}/single/{id}`, `${domain}.SingleAction`, [], '', false);
+        Route.get(`/${prefix}`, `${domain}/ListAction`);
+        Route.get(`/${prefix}/single`, `${domain}/SingleAction`);
+        Route.post(`/${prefix}/create`, `${domain}/CreateAction`);
+        Route.get(`/${prefix}/edit`, `${domain}/EditAction`);
+        Route.post(`/${prefix}/update`, `${domain}/UpdateAction`);
+        Route.post(`/${prefix}/patch`, `${domain}/PatchAction`);
+        Route.get(`/${prefix}/delete`, `${domain}/DeleteAction`);
+        Route.get(`/${prefix}/search`, `${domain}/SearchAction`);
     }
 }
 
