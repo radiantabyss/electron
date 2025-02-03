4b8d60ec9685d063b2a90f33d71f93c27636111c
Robert Buchi <robert@planetweb.ro>
2025-02-03 17:42:30 +0200

    item helpers fix

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -4,8 +4,8 @@ import Invoke from './Invoke.js';
 import Validator from './Validator.js';
 
 import Helpers from './Support/Helpers.js';
-import Item from './Support/Item';
-import Items from './Support/Items';
+import Item from './Support/Item.js';
+import Items from './Support/Items.js';
 import Str from './Support/Str.js';
 
 export default async () => {
